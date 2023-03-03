ccf8a707c9df54d4984e5f17f2d3c6f44c690829
Gabriel <y>
2023-03-03 18:09:22 -0500

    base pygame script

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import pygame
+
+pygame.font.init()
+FPS = 60
+WIDTH, HEIGHT = 700, 584
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+clock = pygame.time.Clock()
+
+
+def draw_update():
+    clock.tick(FPS)
+    pygame.display.flip()
+    WIN.fill((0, 0, 0))
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+
+    draw_update()
